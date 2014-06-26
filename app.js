4bd55996d343b8e19721f3eeb4918b7b5fac3d58
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-26 20:01:13 +0300

    times

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
-var listMatches = function(matches){};
-$(document).ready(function(){
-    $.getJSON('http://worldcup.sfg.io/matches/today', function (matchesToday) {
-        
+var changeTime = function (matches) {
+    matches.forEach(function (match, i) {
+        var date = new Date(match.datetime);
+        var newTime = date.toLocaleTimeString();
+        matches[i].startTime = newTime;
+
     });
+    return matches;
+};
+
+var editProgressTime = function (matches) {
+    matches.forEach(function (match, i) {
+        if (match.status === "in progress") {
+            var date = new Date(match.datetime);
+            var matchTime = date.getTime();
+            var current = new Date();
+            var currentTime = current.getTime();
+
+            var progress = currentTime - matchTime;
+            progress = parseInt(progress/(1000*60));
+            matches[i].progressTime = progress;
+        }
+    });
+    return matches;
+}
+
+$(document).ready(function () {
+    $.getJSON('http://worldcup.sfg.io/matches/today', function (matches) {
+        matches = matches;
+        matches = changeTime( matches);
+        matches= editProgressTime(matches);
+        console.log(matches);
+
+        setInterval(function () {
+         matches = editProgressTime(matches);
+            console.log(matches);
+        }, 1000*60);
+    });
+
+
 });
