cf9a0aaa580568747496cd8dd81d17239ceb9078
smo93 <georgimpeev@gmail.com>
2014-06-26 21:54:44 +0300

    added team info JSON request and popup triggers

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var changeTime = function (matches) {
     matches.forEach(function (match, i) {
-        var date = new Date(match.datetime);
-        var newTime = date.toLocaleTimeString();
-        newTime = newTime.slice(0, newTime.length - 3)
+        var date = new Date(match.datetime),
+            newTime = date.toLocaleTimeString();
+
+        newTime = newTime.slice(0, newTime.length - 3);
         matches[i].startTime = newTime;
 
     });
@@ -12,20 +13,22 @@ var changeTime = function (matches) {
 var editProgressTime = function (matches) {
     matches.forEach(function (match, i) {
         if (match.status === "in progress") {
-            var date = new Date(match.datetime);
-            var matchTime = date.getTime();
-            var current = new Date();
-            var currentTime = current.getTime();
+            var date = new Date(match.datetime),
+                matchTime = date.getTime(),
+                current = new Date(),
+                currentTime = current.getTime(),
+                progress = currentTime - matchTime;
 
-            var progress = currentTime - matchTime;
             progress = parseInt(progress/(1000*60));
             matches[i].progressTime = progress;
+
             $("#minutesLeft").text(progress);
-            console.log($("#minutesLeft"));
+        } else if (match.status === "completed") {
+            matches[i].completed = true;
         }
     });
     return matches;
-}
+};
 
 var loadMatches = function(template, matches ){
     var result = "";
@@ -36,17 +39,34 @@ matches.forEach(function(match){
 };
 
 $(document).ready(function () {
-    var source   = $("#result-block").html();
-    var template = Handlebars.compile(source);
+    var source   = $("#result-block").html(),
+        template = Handlebars.compile(source),
+        pop = $('#info-pop').html(),
+        pop_template = Handlebars.compile(pop);
+
     $.getJSON('http://worldcup.sfg.io/matches/today', function (matches) {
         matches = changeTime( matches);
         matches= editProgressTime(matches);
-        console.log(matches);
+
         $("#main").append(loadMatches(template, matches));
 
+        $.getJSON('http://worldcup.sfg.io/teams/results', function(teams) {
+            console.log(teams);
+            $('.flag').each(function() {
+                var $this = $(this),
+                    country = $this.data('country'),
+                    popover_content = pop_template(teams.filter(function(x) {
+                        return x.country == country; })[0]);
+
+                $this.data('content', popover_content);
+                $this.popover({toggle : 'click'});
+            });
+            console.log('done');
+        });
+
+
         setInterval(function () {
-         matches = editProgressTime(matches);
+            matches = editProgressTime(matches);
         }, 60000);
     });
-
 });
